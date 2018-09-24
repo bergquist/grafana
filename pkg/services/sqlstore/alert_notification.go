24e8964234d2e40c95b29c76cfc76d28b8591e42
bergquist <carl.bergquist@gmail.com>
2018-09-24 16:16:10 +0200

    alerting: move all notification conditions to defaultShouldNotify

--- pkg/services/sqlstore/alert_notification.go
+++ pkg/services/sqlstore/alert_notification.go
@@ -246,18 +246,16 @@ func RecordNotificationJournal(ctx context.Context, cmd *m.RecordNotificationJou
 
 func GetLatestNotification(ctx context.Context, cmd *m.GetLatestNotificationQuery) error {
 	return inTransactionCtx(ctx, func(sess *DBSession) error {
-		nj := &m.AlertNotificationJournal{}
+		nj := []m.AlertNotificationJournal{}
 
-		_, err := sess.Desc("alert_notification_journal.sent_at").
-			Limit(1).
-			Where("alert_notification_journal.org_id = ? AND alert_notification_journal.alert_id = ? AND alert_notification_journal.notifier_id = ?", cmd.OrgId, cmd.AlertId, cmd.NotifierId).Get(nj)
+		err := sess.Desc("alert_notification_journal.sent_at").
+			Where("alert_notification_journal.org_id = ?", cmd.OrgId).
+			Where("alert_notification_journal.alert_id = ?", cmd.AlertId).
+			Where("alert_notification_journal.notifier_id = ?", cmd.AlertId).
+			Find(&nj)
 
 		if err != nil {
 			return err
-		}
-
-		if nj.AlertId == 0 && nj.Id == 0 && nj.NotifierId == 0 && nj.OrgId == 0 {
-			return m.ErrJournalingNotFound
 		}
 
 		cmd.Result = nj
